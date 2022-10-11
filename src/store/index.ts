34769c24ac8ac587afff4fc6ea089fa06fff57dd
shizhe <1076020035@qq.com>
2022-10-11 14:41:30 +0800

    feat: store模块拆分修改

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@
  * @Description:
  * @Author: shizhe
  * @Date: 2022-09-29 11:25:09
- * @LastEditTime: 2022-09-30 14:23:10
+ * @LastEditTime: 2022-09-30 15:11:56
  * @LastEditors: shizhe
  * @Reference:
  */
 
-import store from './modules/store'
+import useUserStore from './modules/userStore'
 export default function useStore() {
-  return store
+  return {
+    userStore: useUserStore
+  }
 }
